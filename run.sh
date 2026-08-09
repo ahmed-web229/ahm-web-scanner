1dea965030368b1d630910c0cce8c1c1c0226329
ahmed-web229 <ahmedziyad555@gmail.com>
2026-08-08 20:36:17 -0700

    run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "[*] Checking Python environment and dependencies..."
-python3 -m pip install -r requirements.txt --quiet
+python3 -m pip install -r requirements.txt --quiet --break-system-packages
 
 echo "[*] Launching AHM Web Scanner..."
 python3 main.py
