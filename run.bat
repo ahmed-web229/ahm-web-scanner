3e95aa88b81af47e039b4b7c59d4a44893842c8c
ahmed-web229 <ahmedziyad555@gmail.com>
2026-08-18 19:09:19 -0700

    run.bat

--- run.bat
+++ run.bat
@@ -1,6 +1,8 @@
 @echo off
+title AHM Web Scanner
 echo [*] Installing dependencies for AHM Web Scanner...
-pip install -r requirements.txt
+python -m pip install -r requirements.txt --quiet
+echo.
 echo [*] Starting AHM Web Scanner...
 python main.py
 pause
